6ed459b3e2849b8cf744ff4df686112f9c4b4a20
tdnq <2397105389@qq.com>
2021-10-07 16:25:44 +0800

    add cube material

---
+++ webapp/src/webgl_page/material/cube.js
@@ -0,0 +1,33 @@
+//    v7----- v6
+//   /|      /|
+//  v3------v2|
+//  | |     | |
+//  | |v4---|-|v5
+//  |/      |/
+//  v0------v1
+
+let sideLength = 0.8;
+let cube = {
+    vertex: [
+        -sideLength, -sideLength, sideLength,
+        sideLength, -sideLength, sideLength,
+        sideLength, sideLength, sideLength,
+        -sideLength, sideLength, sideLength,
+        -sideLength, -sideLength, -sideLength,
+        sideLength, -sideLength, -sideLength,
+        sideLength, sideLength, -sideLength,
+        -sideLength, sideLength, -sideLength,
+
+    ],
+    indices: [
+        0, 3, 2, 0, 1, 2,
+        1, 2, 5, 2, 5, 6,
+        2, 3, 6, 3, 6, 7,
+        0, 3, 4, 3, 4, 7,
+        0, 1, 4, 1, 4, 5,
+        4, 5, 6, 5, 6, 7
+    ],
+    unit: 3
+}
+
+export default cube;
