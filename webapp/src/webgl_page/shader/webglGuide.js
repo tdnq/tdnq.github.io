92a975fd482148230ec28ecc8c8be23c4c6000ee
tdnq <2397105389@qq.com>
2021-10-07 20:50:13 +0800

    rotate use mouse

--- webapp/src/webgl_page/shader/webglGuide.js
+++ webapp/src/webgl_page/shader/webglGuide.js
@@ -1,15 +1,89 @@
 import Base from "../../utils/webglBase";
+import cube from "../material/cube"
+import { rotateCubeUseMouseShaderSource } from "../source_shader/index";
+// import Matrix from "../../utils/matrix.js";
+import { Matrix4 } from "../../utils/matrix_lib.js";
+export default class WebglGuide extends Base {
 
-
-export default class WebglGuide extends Base{
-
-    constructor(...props){
+    constructor(...props) {
 
         super(...props);
 
     }
-    hello(ele){
+    rotateCubeUseMouse(ele) {
         this.canvasContainer = ele.current;
-        this.createCanvas("100%", "100%").appendCanvas()
+        this.createCanvas(this.canvasContainer.clientWidth, this.canvasContainer.clientHeight).appendCanvas().getWebglContext();
+        this.gl.clearColor(1, 1, 1, 0.1);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        //initShader
+        this.initShader(rotateCubeUseMouseShaderSource);
+        //modeling
+        this.rotateCubeUseMouse_model();
+        //transform
+        let viewMatrix = new Matrix4();
+        viewMatrix.setPerspective(30.0, this.ele.width / this.ele.height, 1.0, 100.0);
+        viewMatrix.lookAt(6.0, 4.0, 6.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+        let u_viewMatrix = this.gl.getUniformLocation(this.gl._program, "u_transformMatrix");
+        //bind event
+        this.rotateCubeUseMouse_bind(viewMatrix, u_viewMatrix);
+    }
+    rotateCubeUseMouse_model() {
+        let vertex = new Float32Array(cube.vertex);
+        let pointNum = vertex.length / cube.unit;
+        let indices = new Uint8Array(cube.indices);
+        let color = new Float32Array(this.getRandomArray(4 * pointNum));
+        this.fillShaderAttribution(vertex, "a_position", cube.unit);
+        this.fillShaderAttribution(color, "a_color", 4);
+        this.fillElementsIndex(indices);
+    }
+    rotateCubeUseMouse_bind(viewMatrix, u_viewMatrix) {
+        let isMove = false;
+        let angle = { x: 0, y: 0 };
+        let preX = 0, preY = 0;
+        let g_MvpMatrix = new Matrix4(); // Model view projection matrix
+
+        this.ele.addEventListener("mousedown", (event) => {
+            let elePosition = event.target.getBoundingClientRect();
+            preX = event.clientX - elePosition.x, preY = event.clientY - elePosition.y;
+            isMove = true;
+        });
+        this.ele.addEventListener("mouseup", () => {
+            isMove = false;
+        });
+        this.ele.addEventListener("mousemove", (event) => {
+            if (isMove) {
+                let elePosition = event.target.getBoundingClientRect();
+                //compute the rotate angle
+                let x = event.clientX - elePosition.x,
+                    y = event.clientY - elePosition.y;
+                angle.y += (x - preX) / event.target.width * 180;
+                angle.x += (y - preY) / event.target.height * 180;
+                angle.y = Math.max(Math.min(angle.y, 90.0), -90.0);
+                preX = x, preY = y;
+            }
+        });
+        function tick() {
+            g_MvpMatrix.set(viewMatrix);
+            g_MvpMatrix.rotate(angle.x, 1.0, 0.0, 0.0);
+            g_MvpMatrix.rotate(angle.y, 0.0, 1.0, 0.0);
+            this.gl.uniformMatrix4fv(u_viewMatrix, false, g_MvpMatrix.elements);
+            //rasterization
+            this.gl.clear(this.gl.DEPTH_BUFFER_BIT | this.gl.COLOR_BUFFER_BIT);
+            this.gl.drawElements(this.gl.TRIANGLES, cube.indices.length, this.gl.UNSIGNED_BYTE, 0);
+            window.requestAnimationFrame(bindTick);
+        }
+        let bindTick = tick.bind(this);
+        bindTick();
+    }
+    get_shader_info(name) {
+        let info = {
+            rotateCubeUseMouse: {
+                name: "鼠标控制正方体旋转",
+                describe: "",
+                source: "<<webgl编程指南>>",
+                time: "2021/10/07"
+            }
+        };
+        return info[name]
     }
 }
--- webapp/src/webgl_page/source_shader/index.js
+++ webapp/src/webgl_page/source_shader/index.js
@@ -1,5 +1,5 @@
 
- export const basicLayoutShaderSouce = {
+ export const basicLayoutShaderSource = {
     vshader: `
             attribute vec4 a_position;
             attribute vec4 a_color;
@@ -7,6 +7,27 @@
             void main(){
                 gl_Position = a_position;
                 f_color = a_color;
+            }
+        `,
+    fshader: `
+            precision mediump float;
+            varying vec4 f_color;
+            void main(){
+                gl_FragColor = f_color;
+            }
+        `
+}
+
+export const rotateCubeUseMouseShaderSource={
+        vshader: `
+            attribute vec4 a_position;
+            attribute vec4 a_color;
+            varying vec4 f_color;
+            uniform mat4 u_transformMatrix;
+            void main(){
+                gl_Position = u_transformMatrix * a_position;
+                f_color = a_color;
+                gl_PointSize = 10.0;
             }
         `,
     fshader: `
