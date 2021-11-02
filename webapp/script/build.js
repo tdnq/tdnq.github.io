10bde13c01396d3bc8bddf3c5e1a3bfaaefab5d2
tdnq <2397105389@qq.com>
2021-11-02 23:37:51 +0800

    modify can't refresh problem

--- webapp/script/build.js
+++ webapp/script/build.js
@@ -2,9 +2,10 @@ let fsExtra = require("fs-extra");
 let path = require("path");
 
 
-let rootDir = path.resolve(__dirname,"../../docs");
+let rootDir = path.resolve(__dirname, "../../docs");
 
 fsExtra.emptyDirSync(rootDir);
 
-fsExtra.copySync(path.resolve(__dirname,"../build"),rootDir);
-fsExtra.removeSync(path.resolve(__dirname,"../build"));
+fsExtra.copySync(path.resolve(__dirname, "../build"), rootDir);
+fsExtra.copyFileSync(path.resolve(__dirname, "../build/index.html"), path.resolve(rootDir, "./404.html"));
+fsExtra.removeSync(path.resolve(__dirname, "../build"));
