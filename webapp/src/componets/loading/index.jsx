d05b0ac7c2a410cba1b57064fd3b69eed734d1c1
tdnq <2397105389@qq.com>
2021-11-03 00:09:40 +0800

    modify loading position

--- webapp/src/componets/loading/index.jsx
+++ webapp/src/componets/loading/index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Spin } from 'antd';
 import Loadable from 'react-loadable';
 
-const  LoadPage =(path) => Loadable({
+const LoadPage = (path) => Loadable({
     loader: () => import(`../../pages/${path}`),
     loading: () => {
-        return <div className="loading">
-            <Spin />
+        return <div className="loading" style={{ display: "flex", height: "100%" }}>
+            <Spin style={{ margin: "auto",transform:"translateY(-200%)" }} />
         </div>
     }
 });
