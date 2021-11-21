cd807bb02b335a4056794dd43434261064a7a3e4
tdnq <2397105389@qq.com>
2021-11-22 00:40:37 +0800

    wegl compatibility check

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch,Redirect } from "react-router-dom";
 import { pagesRouterConfig as pagesConfig } from "./config/router";
 import { getRouteComponent } from "./utils/router";
+import WebglCompatibility from "./utils/webglCompatibility"
 import LoadPage from "./componets/loading/index.jsx";
 import "./App.module.scss"
 
 
 function App() {
+  if(!WebglCompatibility.isWebGLAvailable()){
+    return <h1 style={{textAlign:'center'}}>当前浏览器不支持webgl,请更换到Chrome或当前浏览器高版本</h1>
+  }
   return (
     <Router>
       <Switch>
