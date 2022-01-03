3ca1b6a8ef2beb6be1106dd4957ace377583479a
tdnq <2397105389@qq.com>
2022-01-03 15:01:57 +0800

    add eslint

---
+++ webapp/.eslintrc.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'react/prop-types': ['off'],
+  },
+};
--- webapp/src/componets/hud/index.jsx
+++ webapp/src/componets/hud/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./index.module.scss";
 
-function Hud ( props ) {
+function Hud(props) {
 
-    return(
+    return (
         <div className={styles.hudWrapper}>
-            <div className={ styles.hud }>
+            <div className={styles.hud}>
                 <h2 className="name">{props.info && props.info.name}</h2>
                 <div className="describe">介绍: {props.info && props.info.describe}</div>
                 <div className="source">来源:   {props.info && props.info.source}</div>
